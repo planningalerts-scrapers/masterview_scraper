9997371fe930d17590fd1d77a80e7c53cecc2523
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 16:28:07 +1000

    In case of test failure show result immediately

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  # Make it stop on the first failure. Makes in this case
+  # for quicker debugging
+  config.fail_fast = true
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
