38b02592d30a3b3839b4aa044aee78592fe68600
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 15:35:36 +1000

    Add regression test for bellingen

--- spec/masterview_scraper_spec.rb
+++ spec/masterview_scraper_spec.rb
@@ -1,9 +1,43 @@
+require "timecop"
+
 RSpec.describe MasterviewScraper do
   it "has a version number" do
     expect(MasterviewScraper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".scrape_and_save" do
+    def test_scrape_and_save(authority)
+      File.delete("./data.sqlite") if File.exist?("./data.sqlite")
+
+      VCR.use_cassette(authority) do
+        Timecop.freeze(Date.new(2019, 5, 14)) do
+          MasterviewScraper.scrape_and_save(authority)
+        end
+      end
+
+      expected = if File.exist?("spec/expected/#{authority}.yml")
+                   YAML.safe_load(File.read("spec/expected/#{authority}.yml"))
+                 else
+                   []
+                 end
+      results = ScraperWiki.select("* from data order by council_reference")
+      # TODO: Close scraperwiki connection
+
+      if results != expected
+        # Overwrite expected so that we can compare with version control
+        # (and maybe commit if it is correct)
+        File.open("spec/expected/#{authority}.yml", "w") do |f|
+          f.write(results.to_yaml)
+        end
+      end
+
+      expect(results).to eq expected
+    end
+
+    [:bellingen].each do |authority|
+      it authority do
+        test_scrape_and_save(authority)
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "masterview_scraper"
+require "vcr"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,4 +12,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
 end
