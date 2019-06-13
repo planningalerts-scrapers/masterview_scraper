59abe3d01f0340c85174cecec6a20269c90ca9ed
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 16:27:57 +1000

    Extract methods

--- lib/masterview_scraper/authorities/bellingen.rb
+++ lib/masterview_scraper/authorities/bellingen.rb
@@ -1,6 +1,28 @@
 module MasterviewScraper
   module Authorities
     module Bellingen
+      def self.scrape_detail_page(info_page)
+        details = (info_page/'//*[@id="lblDetails"]')
+
+        council_reference = (info_page/'//*[@id="ctl03_lblHead"]').inner_text.split(' ')[0]
+        {
+          'council_reference' => council_reference,
+          'address'           => (info_page/'//*[@id="lblLand"]').inner_text.strip.split("\n")[0].strip,
+          'description'       => details.at("td").inner_text.split("\r")[1].strip[13..-1],
+          'info_url'          => info_page.uri.to_s,
+          'date_scraped'      => Date.today.to_s,
+          'date_received'     => Date.strptime(details.at("td").inner_html.split("<br>")[1].strip[11..-1], "%d/%m/%Y").to_s
+        }
+      end
+
+      def self.scrape_index_page(page, agent)
+        # Visit each DA page so we can get the details
+        (page/'//*[@id="ctl03_lblData"]').search("a").each do |a|
+          info_page = agent.get(agent.page.uri + URI.parse(a.attributes['href']))
+          yield scrape_detail_page(info_page)
+        end
+      end
+
       def self.scrape_and_save
         agent = Mechanize.new
 
@@ -15,21 +37,7 @@ module MasterviewScraper
         # Get the page again
         page = agent.get(url)
 
-        # Visit each DA page so we can get the details
-        (page/'//*[@id="ctl03_lblData"]').search("a").each do |a|
-          info_page = agent.get(agent.page.uri + URI.parse(a.attributes['href']))
-          details = (info_page/'//*[@id="lblDetails"]')
-
-          council_reference = (info_page/'//*[@id="ctl03_lblHead"]').inner_text.split(' ')[0]
-          record = {
-            'council_reference' => council_reference,
-            'address'           => (info_page/'//*[@id="lblLand"]').inner_text.strip.split("\n")[0].strip,
-            'description'       => details.at("td").inner_text.split("\r")[1].strip[13..-1],
-            'info_url'          => info_page.uri.to_s,
-            'date_scraped'      => Date.today.to_s,
-            'date_received'     => Date.strptime(details.at("td").inner_html.split("<br>")[1].strip[11..-1], "%d/%m/%Y").to_s
-          }
-
+        scrape_index_page(page, agent) do |record|
           #    puts record
           puts "Saving record " + record['council_reference']
           ScraperWiki.save_sqlite(['council_reference'], record)
