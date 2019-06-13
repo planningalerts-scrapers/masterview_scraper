32e342ba1e0bf4f10d2ebefb4f0cd3d772988146
Matthew Landauer <mlandauer@gmail.com>
2019-06-14 05:55:16 +1000

    Generalise table extraction to work for tables without proper header and body

--- lib/masterview_scraper/table.rb
+++ lib/masterview_scraper/table.rb
@@ -4,13 +4,44 @@ module MasterviewScraper
   # Utility for getting stuff out of html tables
   module Table
     def self.extract_table(table)
-      headers = table.at("thead").search("th").map(&:inner_text)
-      table.at("tbody").search("tr").map do |tr|
+      headers = header_elements(table).map { |th| th.inner_text.strip }
+      body_rows(table).map do |tr|
         row = tr.search("td").map { |td| td.inner_text.strip }
         {
           url: tr.at("a")["href"],
           content: headers.zip(row).to_h
         }
+      end
+    end
+
+    def self.header_elements(table)
+      # If it's actually got a header use that
+      if table.at("thead")
+        table.at("thead").search("th")
+      # Otherwise just use the first row
+      else
+        table.at("tr").search("td")
+      end
+    end
+
+    def self.header_row(table)
+      # If it's actually got a header use that
+      if table.at("thead")
+        table.at("thead").at("tr")
+      # Otherwise just use the first row
+      else
+        table.at("tr")
+      end
+    end
+
+    def self.body_rows(table)
+      # If it's actually got a body use that
+      if table.at("tbody")
+        table.at("tbody").search("tr")
+      # Otherwise assume the first row is the header and
+      # return everything else
+      else
+        table.search("tr")[1..-1]
       end
     end
   end
