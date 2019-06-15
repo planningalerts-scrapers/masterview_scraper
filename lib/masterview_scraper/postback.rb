d48bcfdb173ecd6c3c2373ea828bd95cc6c202f7
Matthew Landauer <mlandauer@gmail.com>
2019-06-15 12:15:00 +1000

    Use guards

--- lib/masterview_scraper/postback.rb
+++ lib/masterview_scraper/postback.rb
@@ -5,17 +5,15 @@ module MasterviewScraper
   module Postback
     # Implement a click on a link that understands stupid asp.net doPostBack
     def self.click(doc, page)
-      if doc["onclick"] =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
-        form = page.form_with(id: "aspnetForm")
-        form["__EVENTTARGET"] = Regexp.last_match(1)
-        form["__EVENTARGUMENT"] = Regexp.last_match(2)
-        form.submit
-      elsif doc["onclick"] =~ /return false;__doPostBack\('(.*)','(.*)'\)/
-        nil
-      else
-        # TODO: Just follow the link likes it's a normal link
-        raise
-      end
+      return if doc["onclick"] =~ /return false;/
+
+      # TODO: Just follow the link likes it's a normal link
+      raise unless doc["onclick"] =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+
+      form = page.form_with(id: "aspnetForm")
+      form["__EVENTTARGET"] = Regexp.last_match(1)
+      form["__EVENTARGUMENT"] = Regexp.last_match(2)
+      form.submit
     end
   end
 end
