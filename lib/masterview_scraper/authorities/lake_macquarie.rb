46efaf24841d87d79d3cfba5faa73b6d885e34be
Matthew Landauer <mlandauer@gmail.com>
2019-06-18 00:35:43 +1000

    Extract method

--- lib/masterview_scraper/authorities/lake_macquarie.rb
+++ lib/masterview_scraper/authorities/lake_macquarie.rb
@@ -19,17 +19,23 @@ module MasterviewScraper
 
         page = agent.get(url)
 
+        # It looks like this index page doesn't have pagination
+        scrape_index_page(page) do |record|
+          MasterviewScraper.save(record)
+        end
+      end
+
+      def self.scrape_index_page(page)
         table = page.at("#_ctl5_lblData").at("table")
         Table.extract_table(table).each do |row|
-          record = {
+          yield(
             "council_reference" => row[:content]["Application"],
             "address" => row[:content]["Description"].split("<br>")[0],
             "description" => row[:content]["Description"].split("<br>")[1].strip.split("Description: ")[1],
             "info_url" => (page.uri + row[:url]).to_s,
             "date_scraped" => Date.today.to_s,
             "date_received" => Date.strptime(row[:content]["Date Lodged"], "%d/%m/%Y").to_s
-          }
-          MasterviewScraper.save(record)
+          )
         end
       end
     end
