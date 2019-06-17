0fd0837fe542af939583a0497ddc75124582f516
Matthew Landauer <mlandauer@gmail.com>
2019-06-17 14:11:59 +1000

    Extract method

--- lib/masterview_scraper/authorities/hawkesbury.rb
+++ lib/masterview_scraper/authorities/hawkesbury.rb
@@ -30,8 +30,7 @@ module MasterviewScraper
         end
       end
 
-      def self.scrape_and_follow_next_link(doc)
-        scrape_table(doc)
+      def self.next_page(doc)
         nextButton = doc.at('.rgPageNext')
         unless nextButton.nil? || nextButton['onclick'] =~ /return false/
           form = doc.forms.first
@@ -45,9 +44,14 @@ module MasterviewScraper
             'ctl00%24cphContent%24ctl00%24ctl00%24cphContent%24ctl00%24Radajaxpanel2Panel%7Cctl00%24cphContent%24ctl00%24ctl00%24RadGrid1%24ctl00%24ctl03%24ctl01%24ctl10'
           form['ctl00_RadScriptManager1_HiddenField']=
             '%3B%3BSystem.Web.Extensions%2C%20Version%3D3.5.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D31bf3856ad364e35%3Aen-US%3A0d787d5c-3903-4814-ad72-296cea810318%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C%20Version%3D2009.1.527.35%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A1e3fef00-f492-4ed8-96ce-6371bc241e1c%3A16e4e7cd%3Af7645509%3A24ee1bba%3Ae330518b%3A1e771326%3Ac8618e41%3A4cacbc31%3A8e6f0d33%3Aed16cbdc%3A58366029%3Aaa288e2d'
-          doc = form.submit(form.button_with(:name => nextButton['name']))
-          scrape_and_follow_next_link(doc)
+          form.submit(form.button_with(:name => nextButton['name']))
         end
+      end
+
+      def self.scrape_and_follow_next_link(doc)
+        scrape_table(doc)
+        doc = next_page(doc)
+        scrape_and_follow_next_link(doc) if doc
       end
 
       def self.url
