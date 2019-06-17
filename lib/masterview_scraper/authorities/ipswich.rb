d41daf8a583388119b21488ba14909155a1e612b
Matthew Landauer <mlandauer@gmail.com>
2019-06-17 16:52:43 +1000

    Use library

--- lib/masterview_scraper/authorities/ipswich.rb
+++ lib/masterview_scraper/authorities/ipswich.rb
@@ -22,22 +22,6 @@ module MasterviewScraper
         end
       end
 
-      # Implement a click on a link that understands stupid asp.net doPostBack
-      def self.click(page, doc)
-        href = doc["href"]
-        if href =~ /javascript:__doPostBack\(\'(.*)\',\'(.*)'\)/
-          event_target = $1
-          event_argument = $2
-          form = page.form_with(id: "aspnetForm")
-          form["__EVENTTARGET"] = event_target
-          form["__EVENTARGUMENT"] = event_argument
-          form.submit
-        else
-          # TODO Just follow the link likes it's a normal link
-          raise
-        end
-      end
-
       def self.scrape_and_save
         url = "http://pdonline.ipswich.qld.gov.au/pdonline/modules/applicationmaster/default.aspx"
 
@@ -68,7 +52,7 @@ module MasterviewScraper
           end
           if next_page_link
             current_page_no += 1
-            page = click(page, next_page_link)
+            page = Postback.click(next_page_link, page)
           end
         end
       end
