1d57add0fe632d14b1d51de174bff5244423ca99
Matthew Landauer <mlandauer@gmail.com>
2019-06-17 17:58:48 +1000

    Use a bit more of the library

--- lib/masterview_scraper/authorities/lake_macquarie.rb
+++ lib/masterview_scraper/authorities/lake_macquarie.rb
@@ -19,24 +19,17 @@ module MasterviewScraper
 
         page = agent.get(url)
 
-        page.search("table.border").search("tr")[1].search("tr").each do |tr|
-          if ( tr.search("td").count == 4 )
-            tds = tr.search("td")
-            if ( tds[1].inner_text.strip != 'Application' )
-
-              day, month, year = tds[2].inner_text.strip.split("/").map{|s| s.to_i}
-              record = {
-                "council_reference" => tds[1].inner_text.strip,
-                "address" => tds[3].inner_html.split("<br>")[0].strip,
-                "description" => tds[3].inner_html.split("<br>")[1].strip.split("Description: ")[1],
-                "info_url" => (page.uri + tds[0].at('a')["href"]).to_s,
-                "date_scraped" => Date.today.to_s,
-                "date_received" => Date.new(year, month, day).to_s
-              }
-
-              MasterviewScraper.save(record)
-            end
-          end
+        table = page.at("#_ctl5_lblData").at("table")
+        Table.extract_table(table).each do |row|
+          record = {
+            "council_reference" => row[:content]["Application"],
+            "address" => row[:content]["Description"].split("<br>")[0],
+            "description" => row[:content]["Description"].split("<br>")[1].strip.split("Description: ")[1],
+            "info_url" => (page.uri + row[:url]).to_s,
+            "date_scraped" => Date.today.to_s,
+            "date_received" => Date.strptime(row[:content]["Date Lodged"], "%d/%m/%Y").to_s
+          }
+          MasterviewScraper.save(record)
         end
       end
     end
