7b1fc88d7b38495d8c7fb878b0bf10b290f97b25
Matthew Landauer <mlandauer@gmail.com>
2019-06-18 02:41:31 +1000

    Use more of the library

--- lib/masterview_scraper/authorities/logan.rb
+++ lib/masterview_scraper/authorities/logan.rb
@@ -4,24 +4,6 @@ require 'mechanize'
 module MasterviewScraper
   module Authorities
     module Logan
-      def self.scrape_page(page)
-        page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
-          tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
-          day, month, year = tds[2].split("/").map{|s| s.to_i}
-          record = {
-            "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
-            "council_reference" => tds[1],
-            "date_received" => Date.new(year, month, day).to_s,
-            "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].to_s.squeeze(" ").strip,
-            "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
-            "date_scraped" => Date.today.to_s
-          }
-          puts "Saving record " + record['council_reference'] + " - " + record['address']
-      #      puts record
-          ScraperWiki.save_sqlite(['council_reference'], record)
-        end
-      end
-
       def self.scrape_and_save
         url = MasterviewScraper.url_last_14_days(
           "http://pdonline.logan.qld.gov.au/MasterViewUI/Modules/ApplicationMaster",
@@ -35,10 +17,10 @@ module MasterviewScraper
         page = Pages::TermsAndConditions.click_agree(page)
 
         while page
-          scrape_page(page)
-
-          next_page_link = page.at(".rgPageNext")
-          page = Postback.click(next_page_link, page)
+          Pages::Index.scrape(page) do |record|
+            MasterviewScraper.save(record)
+          end
+          page = Pages::Index.next(page)
         end
       end
     end
