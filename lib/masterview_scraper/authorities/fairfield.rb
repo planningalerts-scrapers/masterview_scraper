ac0914774be31fcd97654f4066286d485e2c6e35
Matthew Landauer <mlandauer@gmail.com>
2019-06-17 12:56:43 +1000

    Use more of the library

--- lib/masterview_scraper/authorities/fairfield.rb
+++ lib/masterview_scraper/authorities/fairfield.rb
@@ -9,25 +9,6 @@ module MasterviewScraper
   module Authorities
     # Scraper for Fairfield
     module Fairfield
-
-      def self.scrape_index_page(page)
-        table = page.at("table")
-        data = Table.extract_table(table)
-        data.each do |row|
-          record = {
-            "info_url" => (page.uri + row[:url]).to_s,
-            "council_reference" => row[:content]["Number"],
-            "date_received" => Date.strptime(row[:content]["Submitted"], "%d/%m/%Y").to_s,
-            # TODO: Do proper html entity conversion
-            "description" => row[:content]["Details"].split("<br>")[1].gsub("&amp;", "&"),
-            "address" => row[:content]["Details"].split("<br>")[0],
-            "date_scraped" => Date.today.to_s
-          }
-
-          yield record
-        end
-      end
-
       def self.url
         MasterviewScraper.url_last_14_days(
           "https://openaccess.fairfieldcity.nsw.gov.au/OpenAccess/Modules/Applicationmaster",
@@ -54,7 +35,7 @@ module MasterviewScraper
         page = agent.get(url)
 
         while page
-          scrape_index_page(page) do |record|
+          Pages::Index.scrape(page) do |record|
             yield record
           end
           page = next_index_page(page)
