e054514762d14657587fa5ab781982d2812a64fd
Matthew Landauer <mlandauer@gmail.com>
2019-06-17 15:56:00 +1000

    Add some more helpful error messages

--- lib/masterview_scraper/postback.rb
+++ lib/masterview_scraper/postback.rb
@@ -6,10 +6,14 @@ module MasterviewScraper
     # Implement a click on a link that understands stupid asp.net doPostBack
     def self.click(doc, page)
       js = doc["onclick"] || doc["href"]
+      raise "Couldn't find one of the expected attributes" if js.nil?
+
       return if js =~ /return false;/
 
       # TODO: Just follow the link likes it's a normal link
-      raise unless js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+      unless js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+        raise "Expected a javascript postback link here: #{js}"
+      end
 
       form = page.form_with(id: "aspnetForm")
       form["__EVENTTARGET"] = Regexp.last_match(1)
