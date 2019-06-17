846bdda097b3de8fe71e13241753699c6df8c78e
Matthew Landauer <mlandauer@gmail.com>
2019-06-18 03:55:53 +1000

    Extract methods and new method to check if we're on the t&c page

--- lib/masterview_scraper/pages/terms_and_conditions.rb
+++ lib/masterview_scraper/pages/terms_and_conditions.rb
@@ -6,19 +6,33 @@ module MasterviewScraper
     module TermsAndConditions
       def self.click_agree(page)
         # Click the Agree button on the form
-        form = if page.forms.count == 1
-                 page.forms[0]
-               else
-                 page.form_with(id: "aspnetForm") ||
-                   page.form_with(id: "frmApplicationMaster") ||
-                   page.form_with(id: "frmMasterView")
-               end
+        form = form(page)
         raise "Couldn't find form" if form.nil?
 
-        button = form.button_with(value: /Agree/)
+        button = button(form)
         raise "Can't find agree button" if button.nil?
 
         form.submit(button)
+      end
+
+      # Check if we're actually on this page
+      def self.on_page?(page)
+        form = form(page)
+        form && button(form)
+      end
+
+      def self.form(page)
+        if page.forms.count == 1
+          page.forms[0]
+        else
+          page.form_with(id: "aspnetForm") ||
+            page.form_with(id: "frmApplicationMaster") ||
+            page.form_with(id: "frmMasterView")
+        end
+      end
+
+      def self.button(form)
+        form.button_with(value: /Agree/)
       end
     end
   end
