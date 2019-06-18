8bd9edc2cc1b4e3f69f94957e9044cdf8ada083e
Matthew Landauer <mlandauer@gmail.com>
2019-06-18 13:16:00 +1000

    Update feed to be last 30 days

--- lib/masterview_scraper/authorities/toowoomba.rb
+++ lib/masterview_scraper/authorities/toowoomba.rb
@@ -7,9 +7,8 @@ module MasterviewScraper
   module Authorities
     module Toowoomba
       def self.scrape_and_save
-        url = MasterviewScraper.url_with_period(
+        url = MasterviewScraper.url_last_30_days(
           "https://pdonline.toowoombarc.qld.gov.au/Masterview/Modules/ApplicationMaster",
-          "thisweek",
           "4a" => "\'488\',\'487\',\'486\',\'495\',\'521\',\'540\',\'496\',\'562\'",
           "6" => "F"
         )
