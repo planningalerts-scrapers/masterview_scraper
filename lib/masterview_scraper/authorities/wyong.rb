74193a38bfd8d75aa497367362d1e512a3cd8f92
Matthew Landauer <mlandauer@gmail.com>
2019-06-18 13:27:58 +1000

    Use library

--- lib/masterview_scraper/authorities/wyong.rb
+++ lib/masterview_scraper/authorities/wyong.rb
@@ -25,48 +25,27 @@ module MasterviewScraper
             "date_scraped" => Date.today.to_s
           }
 
-          puts "Saving record " + record['council_reference'] + " - " + record['address']
-      #       puts record
-          ScraperWiki.save_sqlite(['council_reference'], record)
-
-        end
-      end
-
-      # Implement a click on a link that understands stupid asp.net doPostBack
-      def self.click(page, doc)
-        js = doc["href"] || doc["onclick"]
-        if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
-          event_target = $1
-          event_argument = $2
-          form = page.form_with(id: "aspnetForm")
-          form["__EVENTTARGET"] = event_target
-          form["__EVENTARGUMENT"] = event_argument
-          form.submit
-        elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
-          nil
-        else
-          # TODO Just follow the link likes it's a normal link
-          raise
+          MasterviewScraper.save(record)
         end
       end
 
       def self.scrape_and_save
-        url = "http://wsconline.wyong.nsw.gov.au/applicationtracking/modules/applicationmaster/default.aspx?page=found&1=thisweek&4a=437&5=T"
+        url = MasterviewScraper.url_with_period(
+          "http://wsconline.wyong.nsw.gov.au/applicationtracking/modules/applicationmaster",
+          "thisweek",
+          "4a" => "437",
+          "5" => "T"
+        )
 
         agent = Mechanize.new
 
-        # Read in a page
         page = agent.get(url)
+        Pages::TermsAndConditions.click_agree(page)
 
-        # This is weird. There are two forms with the Agree / Disagree buttons. One of them
-        # works the other one doesn't. Go figure.
-        form = page.forms.first
-        button = form.button_with(value: "I Agree")
-        raise "Can't find agree button" if button.nil?
-        form.submit(button)
         # Doesn't redirect
         page = agent.get(url)
 
+        # TODO: Handle paging. Currently ignores it
         scrape_page(page)
       end
     end
