45e88d8902a4e56cf529d7cfa6d2a0a476a32115
Matthew Landauer <mlandauer@gmail.com>
2019-06-19 15:10:47 +1000

    Swap order only

--- lib/masterview_scraper/authorities/shoalhaven.rb
+++ lib/masterview_scraper/authorities/shoalhaven.rb
@@ -28,10 +28,10 @@ module MasterviewScraper
             tr.css('td').collect { |td| td.inner_text.strip }
           end]
           page_info = {}
-          page_info['council_reference'] = item.at_xpath('//title').inner_text.split.first
           page_info['info_url'] = item.at_xpath('//link').inner_text
-          page_info['description'] = item.at_xpath('//title').inner_text.split[2..-1].join(' ')
+          page_info['council_reference'] = item.at_xpath('//title').inner_text.split.first
           page_info['date_received'] = Date.strptime(table_values['Date received:'], '%d %B %Y').to_s
+          page_info['description'] = item.at_xpath('//title').inner_text.split[2..-1].join(' ')
           page_info['address'] = table_values['Address:'] + ', NSW'
           page_info['on_notice_to'] = Date.strptime(table_values['Submissions close:'], '%d %B %Y').to_s
           page_info['date_scraped'] = Date.today.to_s
