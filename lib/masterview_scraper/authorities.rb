102dedc3afd047b0a01363877a55c717dbf6296d
Matthew Landauer <mlandauer@gmail.com>
2019-06-22 21:23:11 +1000

    Add port stephens

--- lib/masterview_scraper/authorities.rb
+++ lib/masterview_scraper/authorities.rb
@@ -162,6 +162,13 @@ module MasterviewScraper
       url: "https://datracker.pmhc.nsw.gov.au",
       period: :last30days,
       use_api: true
+    },
+    port_stephens: {
+      url: "http://datracker.portstephens.nsw.gov.au",
+      period: :thismonth,
+      use_api: true,
+      long_council_reference: true,
+      types: [16, 9, 25]
     }
   }.freeze
 end
