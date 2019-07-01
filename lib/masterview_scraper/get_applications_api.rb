26ad49412148beb37676b8e62d1412c7315ccb69
Matthew Landauer <mlandauer@gmail.com>
2019-07-01 14:59:16 +1000

    Don't error if there are no records returned

--- lib/masterview_scraper/get_applications_api.rb
+++ lib/masterview_scraper/get_applications_api.rb
@@ -46,23 +46,25 @@ module MasterviewScraper
       )
 
       result = JSON.parse(page.body)
-      raise "Unexpected for of result: #{result}" unless result["data"].respond_to?(:each)
 
-      result["data"].each do |application|
-        details = application[4].split("<br/>")
-        # TODO: Do this properly
-        description = details[-1].gsub("<b>", "").gsub("</b>", "").squeeze(" ")
-        # If no description then use the application type as the description
-        description = application[2] if description.empty?
-        yield(
-          "council_reference" => long_council_reference ? application[0] : application[1],
-          # Only picking out the first address
-          "address" => details[0].strip,
-          "description" => description,
-          "info_url" => (page.uri + "ApplicationDetails/" + application[0]).to_s,
-          "date_scraped" => Date.today.to_s,
-          "date_received" => Date.strptime(application[3], "%d/%m/%Y").to_s
-        )
+      # "data" is only populated sensibly if there are records. So check this first
+      if result["recordsTotal"].positive?
+        result["data"].each do |application|
+          details = application[4].split("<br/>")
+          # TODO: Do this properly
+          description = details[-1].gsub("<b>", "").gsub("</b>", "").squeeze(" ")
+          # If no description then use the application type as the description
+          description = application[2] if description.empty?
+          yield(
+            "council_reference" => long_council_reference ? application[0] : application[1],
+            # Only picking out the first address
+            "address" => details[0].strip,
+            "description" => description,
+            "info_url" => (page.uri + "ApplicationDetails/" + application[0]).to_s,
+            "date_scraped" => Date.today.to_s,
+            "date_received" => Date.strptime(application[3], "%d/%m/%Y").to_s
+          )
+        end
       end
 
       # Return the total number of records (not just what's in this page)
